505532ee6799343ddec604b99c96f2a8a585dfec
JunQiu0 <junqiu@ucm.es>
2024-03-05 13:58:09 +0100

    Creating a template for the index page

--- project/placesforu/views.py
+++ project/placesforu/views.py
@@ -1,8 +1,7 @@
 from django.shortcuts import render
 from django.http import HttpResponse
-
 # Create your views here.
 
 #Index page 
 def index(request):
-    return HttpResponse("Hello, django!")
+    return render(request, "placesforu/index.html")
