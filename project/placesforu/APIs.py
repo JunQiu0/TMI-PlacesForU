d3d06ba67878da9fccf8f538f4828f0e428d7c38
Jaimepas77 <jaipas01@ucm.es>
2024-03-05 18:49:25 +0100

    Inicio del backend
    Se incluyen los primeros elementos para consultar la API.

---
+++ project/placesforu/APIs.py
@@ -0,0 +1,13 @@
+
+def get_landmark(image_rute):
+    '''Given a route to a local image returns the location 
+    of the image or (-1, -1) otherwise'''
+    coord = (-1, -1)    #Default coords
+
+    # Use google vision API to find location
+
+
+    # Build json with the info
+    # Coords, city, etc
+
+    return coord
--- project/placesforu/views.py
+++ project/placesforu/views.py
@@ -1,7 +1,14 @@
 from django.shortcuts import render
 from django.http import HttpResponse
+import placesforu.APIs as api
+from django.template import Context
 # Create your views here.
 
 #Index page 
 def index(request):
     return render(request, "placesforu/index.html")
+
+def upload_image(request):
+    coords = api.get_landmark("./tmp_image")
+    context = {"coords": coords}
+    return render(request, "placesforu/coords.html", context)
