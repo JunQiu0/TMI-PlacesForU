eee71912054c411482777e6cfc4efba4f8771ee1
sh4dww <76074604+sh4dww@users.noreply.github.com>
2024-03-11 23:55:34 +0100

    subidor imágenes

--- project/placesforu/models.py
+++ project/placesforu/models.py
@@ -1,3 +1,10 @@
 from django.db import models
+from django.utils.text import slugify 
 
 # Create your models here.
+class UploadImageModel(models.Model):
+	title=models.CharField(max_length=200)
+	img=models.ImageField(upload_to="images/")
+
+	def __str__(self):
+		return self.title
