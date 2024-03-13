3a5720a3c8955d844a53fbfc9dda8e015bd0dcce
Jaime <79403778+Jaimepas77@users.noreply.github.com>
2024-03-13 13:41:44 +0100

    get_landmark (mediante ruta a imagen)
    Interacción con la API de google vision para obtener la ubicación. Se incluye un test también.

--- project/placesforu/APIs.py
+++ project/placesforu/APIs.py
@@ -1,13 +1,37 @@
+from google.cloud import vision
 
-def get_landmark(image_rute):
+def get_landmark(image_path):
     '''Given a route to a local image returns the location 
-    of the image or (-1, -1) otherwise'''
-    coord = (-1, -1)    #Default coords
+    of the image (as a dict) or None otherwise. The server 
+    running must have a token for the google vision API.
+    
+    Structure of the dict:
+    {
+        "latitude": float,
+        "longitude": float,
+        "landmark": "string" (name of the landmark),
+    }
+    '''
+    coord = None    #Default coords
+    path = image_path
 
     # Use google vision API to find location
+    client = vision.ImageAnnotatorClient()
 
+    with open(path, "rb") as image_file:
+        content = image_file.read()
 
-    # Build json with the info
-    # Coords, city, etc
+    image = vision.Image(content=content)
+    response = client.landmark_detection(image=image)
+    landmarks = response.landmark_annotations
+
+    # Build dict with the info
+    # Coords, landmark name
+    if landmarks:
+        coord = {
+            "latitude": landmarks[0].locations[0].lat_lng.latitude,
+            "longitude": landmarks[0].locations[0].lat_lng.longitude,
+            "landmark": landmarks[0].description,
+        }
 
     return coord
