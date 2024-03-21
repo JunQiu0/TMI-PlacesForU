cc7a205c588ca6babc35367d0843492a51052923
JunQiu0 <junqiu@ucm.es>
2024-03-21 14:16:51 +0100

    Google API de Maps JavaScript
    Nivel funcional

--- project/placesforu/views.py
+++ project/placesforu/views.py
@@ -6,9 +6,7 @@ from django.template import Context
 
 #Index page 
 def index(request):
-    src_map = api.get_map_src(40.45285938607549,-3.7336615037034977)
-    context = {"src_map": src_map}
-    return render(request, "placesforu/index.html", context)
+    return render(request, "placesforu/index.html")
 
 def upload_image(request):
     coords = api.get_landmark("./tmp_image")
