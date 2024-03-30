57324a1fb1ff505a6eee9250384c2255ff3de588
Jaime <79403778+Jaimepas77@users.noreply.github.com>
2024-03-30 12:02:24 +0100

    Hotfix - redirección inicial
    Por defecto, la ruta inicial ahora redirige a /placesforu

--- project/project/urls.py
+++ project/project/urls.py
@@ -16,8 +16,10 @@ Including another URLconf
 """
 from django.contrib import admin
 from django.urls import path, include
+from django.views.generic import RedirectView
 
 urlpatterns = [
     path('admin/', admin.site.urls),
-    path("placesforu", include("placesforu.urls"))
+    path("placesforu/", include("placesforu.urls")),
+    path("", RedirectView.as_view(url="placesforu/")),
 ]
