e6370633a886ff8076834ffc0bfc7d97b1242a25
Gemma <gemmgonz@ucm.es>
2024-04-02 18:21:14 +0200

    introducir url

--- project/placesforu/forms.py
+++ project/placesforu/forms.py
@@ -1,4 +1,7 @@
 from django import forms
  
 class UploadImageForm(forms.Form):
+    image_url = forms.CharField(label='Sube una imagen')
+    image_url.required = False
     image_field = forms.ImageField(label='')#image')
+    image_field.required = False
