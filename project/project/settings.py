15c73b1f412d8a3cb57fd7078fd0b5a84f212f3c
Jaimepas77 <jaipas01@ucm.es>
2024-04-16 18:42:23 +0200

    Error 404 implementado a nivel básico.

--- project/project/settings.py
+++ project/project/settings.py
@@ -24,9 +24,9 @@ BASE_DIR = Path(__file__).resolve().parent.parent
 SECRET_KEY = 'django-insecure-588jbudth(fqck_c-3aa-0_t2ba8p^(li6^xt4@*55@lv^dkfm'
 
 # SECURITY WARNING: don't run with debug turned on in production!
-DEBUG = True
+DEBUG = False
 
-ALLOWED_HOSTS = []
+ALLOWED_HOSTS = ['*']
 
 
 # Application definition
